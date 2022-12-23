886cbcb3aca83a6ed776d9d8439dd135773a9c89
my git btoiandw <bbdbiw2544@gmail.com>
2022-12-23 09:35:15 +0700

    first commit

---
+++ app/Http/Controllers/TbAdminController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tbAdmin;
+use Illuminate\Http\Request;
+
+class TbAdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\tbAdmin  $tbAdmin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(tbAdmin $tbAdmin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\tbAdmin  $tbAdmin
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(tbAdmin $tbAdmin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\tbAdmin  $tbAdmin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, tbAdmin $tbAdmin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\tbAdmin  $tbAdmin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(tbAdmin $tbAdmin)
+    {
+        //
+    }
+}
